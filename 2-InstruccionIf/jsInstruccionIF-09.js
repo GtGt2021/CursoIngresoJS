be94460e19bc270cf69d2a5d5737b6bb0b9d9c0b
GtGt2021 <tecnimakinas@gmail.com>
2021-07-13 22:04:19 -0300

    martes 13

--- 2-InstruccionIf/jsInstruccionIF-09.js
+++ 2-InstruccionIf/jsInstruccionIF-09.js
@@ -1,10 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
+var numero
+const maximo = 10;
+const minimo = 1;
+numero = Math.round(Math.random()*(maximo - minimo) + minimo);
+
+alert(numero)
+/*	//Genero el número RANDOM entre 1 y 10 
 	var rand = Math.random();
 	rand=rand*10;
 	rand=parseInt(rand);
 	rand=rand+1;
 	alert(rand);	
-
+*/
 }
